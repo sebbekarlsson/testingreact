f85df497a182f003981764d52e18ee92221fad36
ianertson <sebbekarlsson97@gmail.com>
2020-04-22 08:58:02 +0200

    react hooks

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 
-const About = () => (
-    <h1>About</h1>
-);
+const About = () => {
+    const  [hasError, setErrors] =  useState(false);
+    const  [names, setNames] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const res = await fetch("/data.json");
+            res
+                .json()
+                .then(res => setNames(res))
+                .catch(err => setErrors(err));
+        }
+
+        fetchData();
+    });
+    
+    return (
+        <div>
+            <h1>About</h1>
+            <ul>
+                {names.map((name) =>{
+                    return (<li>{name}</li>)
+                })}
+            </ul>
+        </div>
+    );
+};
 
 export default About;
